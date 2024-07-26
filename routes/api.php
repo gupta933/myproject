0eb77fac3be68ceacb3063f60c7e2637612a47fc
Rahul kumar <officialmrgupta9@gmail.com>
2024-07-26 15:33:05 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use App\Http\Middleware\ApiKeyMiddleware;
 
-Route::middleware(ApiKeyMiddleware::class)->group(function () {
     Route::post('/todo/add', [TaskController::class, 'addTask']);
     Route::post('/todo/status', [TaskController::class, 'updateStatus']);
-});
+
